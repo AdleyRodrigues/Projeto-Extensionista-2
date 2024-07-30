443e162725f42b7a0cde0dd43261fe30cb5bb24b
Adley Rodrigues <adleyrodrigues2015@gmail.com>
2024-07-29 22:08:15 -0300

    teste final

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,21 +2,12 @@
 //const API_URL = "https://6d71-2804-29b8-5015-2d0e-2444-3a4a-757d-e07f.ngrok-free.app/comments";
 const API_URL = import.meta.env.VITE_API_URL;
 
-
 export const fetchComments = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const text = await response.text(); 
-    console.log(text); 
-    
-    if (!response.ok) {
-      throw new Error("Failed to fetch comments");
-    }
-
-    return JSON.parse(text); 
-  } catch (error) {
-    console.error('Erro ao buscar comentários:', error);
+  const response = await fetch(API_URL);
+  if (!response.ok) {
+    throw new Error("Failed to fetch comments");
   }
+  return response.json();
 };
 
 export const postComment = async (comment: {
